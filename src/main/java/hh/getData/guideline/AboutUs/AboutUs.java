b314a746e7664b3d94084ca80a723ee94573b96c
halhadi <haleemah.hadi@hs-augsburg.de>
2023-09-26 14:46:43 +0200

    Add column

--- src/main/java/hh/getData/guideline/AboutUs/AboutUs.java
+++ src/main/java/hh/getData/guideline/AboutUs/AboutUs.java
@@ -1,4 +1,34 @@
 package hh.getData.guideline.AboutUs;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AboutUs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long about_id;
+
+    @Column(name = "logo", nullable = false)
+    private String logo;
+
+    @Column(name = "about_app", nullable = false)
+    private String about_app;
+
+    @Column(name = "our_services", nullable = false)
+    private String our_services;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phone_number;
+
+    @Column(name = "status", nullable = true)
+    private boolean status;
 }
