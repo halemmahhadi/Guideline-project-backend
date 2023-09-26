0d13344703be4111c89e98a9561d117ea821fe06
halhadi <haleemah.hadi@hs-augsburg.de>
2023-09-26 11:43:34 +0200

    initialize spring boot project

---
+++ src/main/java/hh/getData/guideline/GuidelineApplication.java
@@ -0,0 +1,13 @@
+package hh.getData.guideline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidelineApplication.class, args);
+	}
+
+}
