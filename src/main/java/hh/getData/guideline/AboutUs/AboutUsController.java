97e405cee36f6040f245382faedf97de5758106f
halhadi <haleemah.hadi@hs-augsburg.de>
2023-10-13 23:50:00 +0200

    edit AboutUs api

--- src/main/java/hh/getData/guideline/AboutUs/AboutUsController.java
+++ src/main/java/hh/getData/guideline/AboutUs/AboutUsController.java
@@ -1,19 +1,13 @@
 package hh.getData.guideline.AboutUs;
 
-import hh.getData.guideline.AboutUs.AboutUs;
-import hh.getData.guideline.AboutUs.AboutUsDto;
-import hh.getData.guideline.AboutUs.AboutUsService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import lombok.AllArgsConstructor;
+import hh.getData.guideline.Donors.Donors;
+import hh.getData.guideline.Donors.DonorsDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.function.EntityResponse;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,86 +16,99 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin
-@RequestMapping("/about-us/")
+@RequestMapping("/aboutUs/")
 public class AboutUsController {
 
     private final AboutUsService aboutUsService;
-    @RequestMapping("getAll/")
-    @ApiOperation(value = "view a list of available AboutUs information",
-            notes = "get all AboutUs info",
-            response = List.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success"),
-            @ApiResponse(code = 404, message = "There is no info to display"),
-    })
-    public ResponseEntity<List<AboutUsDto>> getAll(){
-        log.debug("Received request to get all info");
+
+    @GetMapping("getAll/")
+    public ResponseEntity<List<AboutUsDto>> getAll() {
         try {
-            List<AboutUs> aboutUses = aboutUsService.getAll();
-            List<AboutUsDto> aboutUsDto = aboutUses.stream().map(AboutUsDto::from).collect(Collectors.toList());
-            return new ResponseEntity<>(aboutUsDto, HttpStatus.OK);
+            List<AboutUs> aboutUsEntries = aboutUsService.getAllAboutUsEntries();
+            List<AboutUsDto> aboutUsDtoList = aboutUsEntries.stream()
+                    .map(AboutUsDto::from)
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(aboutUsDtoList, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-
         }
-
     }
 
-    // Post
+    @GetMapping("getById/id/{id}")
+    public ResponseEntity<AboutUsDto> getAboutUsById(@PathVariable Long id) {
+        try {
+            AboutUs aboutUs = aboutUsService.getAboutUsById(id);
+            if (aboutUs != null) {
+                return new ResponseEntity<>(AboutUsDto.from(aboutUs), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-    @PostMapping(value = "create/")
-
+    @PostMapping("create/")
     public ResponseEntity<AboutUsDto> addAboutUs(@RequestBody AboutUsDto aboutUsDto) {
         try {
-            AboutUs aboutUs = aboutUsService.createAboutUS(AboutUs.from(aboutUsDto));
+            AboutUs aboutUs = aboutUsService.createAboutUs(AboutUs.from(aboutUsDto));
             return new ResponseEntity<>(AboutUsDto.from(aboutUs), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-
         }
     }
 
-
-    // delete
-
-    @DeleteMapping(value = "delete/id/{id}/")
-    @ApiOperation(value = "Delete aboutUs Info",
-            notes = " deleting info",
-            response = List.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully deleted aboutUs info"),
-            @ApiResponse(code = 404, message = "Can not found the data"),
-
-
-    })
-    public ResponseEntity<Boolean> deleteAboutUs(@PathVariable final Long id) {
+    @DeleteMapping("delete/id/{id}")
+    public ResponseEntity<Boolean> deleteAboutUs(@PathVariable Long id) {
         try {
             return new ResponseEntity<>(aboutUsService.deleteAboutUs(id), HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.OK);
-
         }
     }
 
-
-    //updating
-
-    @PutMapping(value = "update/id/{id}/")
-    @ApiOperation(value = "Update aboutUs",
-            notes = "edit AboutUs by id",
-            response = List.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully updated "),
-            @ApiResponse(code = 404, message = "There is no Data with this ID to update")
-    })
-    public ResponseEntity<AboutUsDto> editAboutUs(@PathVariable final Long id, @RequestBody final AboutUsDto aboutUsDto) {
+    @PutMapping("update/id/{id}")
+    public ResponseEntity<AboutUsDto> editAboutUs(@PathVariable Long id, @RequestBody AboutUsDto aboutUsDto) {
         try {
-            AboutUs editAboutUs = aboutUsService.updateAboutUs(id, AboutUs.from(aboutUsDto));
-            return new ResponseEntity<>(AboutUsDto.from(editAboutUs), HttpStatus.OK);
-        }
-        catch (Exception e){
+            AboutUs editedAboutUs = aboutUsService.updateAboutUs(id, AboutUs.from(aboutUsDto));
+            return new ResponseEntity<>(AboutUsDto.from(editedAboutUs), HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 
+    @PutMapping("inactive/id/{id}")
+    public ResponseEntity<String> changeStatusToInactive(@PathVariable Long id) {
+        try {
+            aboutUsService.changeStatusToInactive(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to change status record with ID " + id, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("active/id/{id}")
+    public ResponseEntity<String> changeStatusToActive(@PathVariable Long id) {
+        try {
+            aboutUsService.changeStatusToActive(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to change status record with ID " + id, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("deleteAll/")
+    public ResponseEntity<String> deleteAllAboutUsEntries() {
+        try {
+            aboutUsService.deleteAllAboutUsEntries();
+            return new ResponseEntity<>("All AboutUs entries have been deleted.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to delete all AboutUs entries.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping(value = "{aboutUsId}/image/{imageId}/add")
+    public ResponseEntity<AboutUsDto> addImage(@PathVariable final Long aboutUsId, @PathVariable final Long imageId) {
+        AboutUs aboutUs = aboutUsService.addImage(aboutUsId, imageId);
+        return new ResponseEntity<>(AboutUsDto.from(aboutUs), HttpStatus.OK);
+    }
 }
--- src/main/java/hh/getData/guideline/AboutUs/AboutUsService.java
+++ src/main/java/hh/getData/guideline/AboutUs/AboutUsService.java
@@ -1,6 +1,11 @@
 package hh.getData.guideline.AboutUs;
 
+
 import hh.getData.guideline.Exception.EntityNotFoundException;
+import hh.getData.guideline.Exception.StatusAlreadyInactiveException;
+import hh.getData.guideline.Image.Image;
+import hh.getData.guideline.Image.ImageService;
+import hh.getData.guideline.enumeration.Status;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,55 +19,105 @@ import java.util.Optional;
 @Transactional
 @Slf4j
 public class AboutUsService {
-     private final AboutUsRepository aboutUsRepository;
+    private final AboutUsRepository aboutUsRepository;
+private final ImageService imageService;
+    public AboutUs createAboutUs(AboutUs newAboutUs) {
+        log.info("Saving new AboutUs information");
+        // Check if an entry already exists
+        if (doesAboutUsExist()) {
+            // Handle the case where an entry already exists (You can throw an exception or return null, depending on your requirements)
+            throw new AboutUsAlreadyExistsException("An AboutUs entry already exists.");
+        }
+        return aboutUsRepository.save(newAboutUs);
+    }
+    public boolean doesAboutUsExist() {
+        return aboutUsRepository.count() > 0;
+    }
 
 
-     public AboutUs createAboutUS(AboutUs newAboutUs){
-         log.info("saving new About_us information: {}", newAboutUs.getAbout_app());
-         return aboutUsRepository.save(newAboutUs);
-     }
-
-    public List<AboutUs> getAll() {
-        log.info("Fetching all");
+    public List<AboutUs> getAllAboutUsEntries() {
+        log.info("Fetching all AboutUs entries");
         return aboutUsRepository.findAll();
     }
 
-    public AboutUs getById(Long id){
-        log.info("Fetching by id: {}", id);
-         return aboutUsRepository.getById(id);
+    public AboutUs getAboutUsById(Long id) {
+        log.info("Fetching AboutUs entry by ID: {}", id);
+        Optional<AboutUs> aboutUs = aboutUsRepository.findById(id);
+        return aboutUs.orElseThrow(() -> new EntityNotFoundException("AboutUs entry with ID " + id + " not found"));
     }
 
-    public AboutUs updateAboutUs(Long id,AboutUs aboutUsUpdate){
-        log.info("updating About_us information: {}", aboutUsUpdate.getAbout_app());
+    public AboutUs updateAboutUs(Long id, AboutUs aboutUsUpdate) {
+        log.info("Updating AboutUs information with ID: {}", id);
 
         Optional<AboutUs> existingAboutUsOptional = aboutUsRepository.findById(id);
 
         if (existingAboutUsOptional.isPresent()) {
             AboutUs existingAboutUs = existingAboutUsOptional.get();
-
             // Update attributes with new values
+            existingAboutUs.setImage(aboutUsUpdate.getImage());
             existingAboutUs.setAbout_app(aboutUsUpdate.getAbout_app());
-            existingAboutUs.setLogo(aboutUsUpdate.getLogo());
             existingAboutUs.setOur_services(aboutUsUpdate.getOur_services());
             existingAboutUs.setEmail(aboutUsUpdate.getEmail());
             existingAboutUs.setPhone_number(aboutUsUpdate.getPhone_number());
-            existingAboutUs.setOur_services(aboutUsUpdate.getOur_services());
-            // Update other attributes as needed
-
             // Save the updated entity
             return aboutUsRepository.save(existingAboutUs);
         } else {
             // Handle the case where the entity with the given ID does not exist
-            throw new EntityNotFoundException("AboutUs with ID " + id + " not found");
+            throw new EntityNotFoundException("AboutUs entry with ID " + id + " not found");
         }
     }
 
-
-    public boolean deleteAboutUs(Long id){
-        log.info("deleting by id: {}", id);
+    public boolean deleteAboutUs(Long id) {
+        log.info("Deleting AboutUs entry by ID: {}", id);
         aboutUsRepository.deleteById(id);
-        return Boolean.TRUE;
+        return true;
     }
 
 
+
+    @Transactional
+    public void changeStatusToInactive(Long id) {
+        try {
+            AboutUs aboutUs = aboutUsRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("AboutUs entry with ID " + id + " not found"));
+
+            if (aboutUs.getStatus() != Status.INACTIVE) {
+                aboutUs.setStatus(Status.INACTIVE);
+                aboutUsRepository.save(aboutUs);
+            } else {
+                throw new StatusAlreadyInactiveException("AboutUs entry with ID " + id + " is already INACTIVE");
+            }
+        } catch (Exception e) {
+            log.error("An error occurred during status changing of AboutUs entry with ID " + id, e);
+        }
+    }
+
+    @Transactional
+    public void changeStatusToActive(Long id) {
+        try {
+            AboutUs aboutUs = aboutUsRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("AboutUs entry with ID " + id + " not found"));
+
+            if (aboutUs.getStatus() != Status.ACTIVE) {
+                aboutUs.setStatus(Status.ACTIVE);
+                aboutUsRepository.save(aboutUs);
+            } else {
+                throw new StatusAlreadyInactiveException("AboutUs entry with ID " + id + " is already ACTIVE");
+            }
+        } catch (Exception e) {
+            log.error("An error occurred during status changing of AboutUs entry with ID " + id, e);
+        }
+    }
+
+    public void deleteAllAboutUsEntries() {
+        aboutUsRepository.deleteAll();
+    }
+
+    @Transactional
+    public AboutUs addImage(Long aboutUsId, Long imageId){
+        AboutUs aboutUs=getAboutUsById(aboutUsId);
+        Image image=imageService.getById(imageId);
+        aboutUs.setImage(image);
+        return aboutUs;
+    }
 }
