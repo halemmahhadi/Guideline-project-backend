a7619a366868d4f85dc39033095c9c4c04462e39
halhadi <haleemah.hadi@hs-augsburg.de>
2023-10-13 23:59:13 +0200

    add exception

---
+++ src/main/java/hh/getData/guideline/AboutUs/AboutUsAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package hh.getData.guideline.AboutUs;
+
+public class AboutUsAlreadyExistsException extends RuntimeException {
+    public AboutUsAlreadyExistsException(String message) {
+        super(message);
+    }
+}
